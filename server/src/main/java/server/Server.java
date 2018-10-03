a52250cc031bd585b85aa44f274ed99877d2e1d1
Michael Bowman <mgbowman@memphis.edu>
2018-10-03 15:25:49 -0500

    Created basic server project

---
+++ server/src/main/java/server/Server.java
@@ -0,0 +1,13 @@
+package server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Server {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApplication.class, args);
+	}
+
+}
