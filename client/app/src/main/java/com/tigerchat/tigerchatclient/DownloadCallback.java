0252e12f84b95446fb6dfb001060498b77a87852
Unknown <giganticwarpanda@gmail.com>
2018-10-04 14:17:58 -0500

    initial-client-commit
    Setup for the client application

---
+++ client/app/src/main/java/com/tigerchat/tigerchatclient/DownloadCallback.java
@@ -0,0 +1,39 @@
+package com.tigerchat.tigerchatclient;
+
+import android.net.NetworkInfo;
+
+interface DownloadCallback <T> {
+    interface Progress {
+        int ERROR = -1;
+        int CONNECT_SUCCESS = 0;
+        int GET_INPUT_STREAM_SUCCESS = 1;
+        int PROCESS_INPUT_STREAM_IN_PROGRESS = 2;
+        int PROCESS_INPUT_STREAM_SUCCESS = 3;
+    }
+
+    /**
+     * Indicates the callback handler needs to update on the result.
+     *
+     */
+    void updateFromDownload(T result);
+
+    /**
+     *  Get the device's active network status in the form of a NetworkInfo object.
+     *
+     */
+    NetworkInfo getActiveNetworkInfo();
+
+    /**
+     * Indicate to callback handler any progress update.
+     * @param progressCode must be one of the constants defined in DownloadCallback.Progress.
+     * @param percentComplete must be 0-100.
+     */
+    void onProgressUpdate(int progressCode, int percentComplete);
+
+    /**
+     * Indicates that the download operation has finished. This method is called even if the
+     * download hasn't completed successfully.
+     */
+    void finishDownloading();
+
+}
